06711ec32ca5d7dc487548cc3af0da01af9d854f
Lee Mracek <lee.mracek@gmail.com>
2016-07-26 15:26:45 -0400

    Fix no accept on java install

--- bootstrap.sh
+++ bootstrap.sh
@@ -53,7 +53,7 @@ vim +PlugInstall +qall
 
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 
-sudo apt-get install oracle-java8-installer oracle-java8-set-default
+sudo apt-get install oracle-java8-installer oracle-java8-set-default -yq
 
 #######################
 #####= Terminals =#####
